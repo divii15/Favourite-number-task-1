e417d89aeba182c164690060ec52adaaffa93682
divii15 <divyadaksha143@gmail.com>
2024-12-28 15:14:07 +0530

    My next task(1)(8) 28/12

---
+++ Task 1 Favourite number.py
@@ -0,0 +1,8 @@
+
+a=int(input("Enter the value: "))
+if(a==1 or a==10 or a==2 or a==8 or a==3):
+    print("It is favourite number")
+elif(a==7 or a==4 or a==6 or a==5 or a==9):
+    print("It is not a favourite number")
+else:
+    print("Invalid Input")
