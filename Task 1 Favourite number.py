963dfa48484fe40ba7d92d0468c9733375f5138f
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:05:05 +0530

    My next task(1)(8)

--- Task 1 Favourite number.py
+++ Task 1 Favourite number.py
@@ -1,3 +1,7 @@
+#TASK 1
+#8)if input is 1 (or) 10 (or) 2 (or) 8 (or) 3 output will be Favorite number
+#if input is 7 (or) 4 (or) 6 (or) 5 (or) 9 output will be not  Favorite number
+
 
 a=int(input("Enter the value: "))
 if(a==1 or a==10 or a==2 or a==8 or a==3):
